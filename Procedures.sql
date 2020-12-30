29d0d55feb5a9b3c27de387323e673d845fec523
tslipa <p.cegiela@outlook.com>
2020-12-30 21:29:36 +0100

    Improvements of triggers and procedures.

--- Procedures.sql
+++ Procedures.sql
@@ -1,40 +1,40 @@
 DELIMITER $$
 
-CREATE OR REPLACE PROCEDURE addCity (city VARCHAR(32))
+CREATE OR REPLACE PROCEDURE addCity(city VARCHAR(32))
 BEGIN
     INSERT INTO citystats (name, population)
     VALUES (city, 0);
 END $$
 
-CREATE OR REPLACE PROCEDURE addYear (year INT)
+CREATE OR REPLACE PROCEDURE addYear(year INT)
 BEGIN
     INSERT INTO yearstats (year, quantity)
     VALUES (year, 0);
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizenPassword(pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE deleteCitizenPassword(delPesel VARCHAR(11))
 BEGIN
-    DELETE FROM passwords P WHERE P.pesel = pesel;
+    DELETE FROM passwords WHERE pesel = delPesel;
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizenStatus(pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE deleteCitizenStatus(delPesel VARCHAR(11))
 BEGIN
-    DELETE FROM statuses S WHERE S.pesel = pesel;
+    DELETE FROM statuses WHERE pesel = delPesel;
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizenGender(pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE deleteCitizenGender(delPesel VARCHAR(11))
 BEGIN
-    DELETE FROM genders G WHERE G.pesel = pesel;
+    DELETE FROM genders WHERE pesel = delPesel;
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizenBirthday(pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE deleteCitizenBirthday(delPesel VARCHAR(11))
 BEGIN
-    DELETE FROM birthdays B WHERE B.pesel = pesel;
+    DELETE FROM birthdays WHERE pesel = delPesel;
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizenAddress(pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE deleteCitizenAddress(delPesel VARCHAR(11))
 BEGIN
-    DELETE FROM addresses A WHERE A.pesel = pesel;
+    DELETE FROM addresses WHERE pesel = delPesel;
 END $$
 
 CREATE OR REPLACE PROCEDURE changeAddress(pesel VARCHAR(11), city VARCHAR(32), street VARCHAR(32), house INT, flat INT)
@@ -65,7 +65,7 @@ BEGIN
     COMMIT;
 END $$
 
-CREATE OR REPLACE PROCEDURE printPersonalData (pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE printPersonalData(pesel VARCHAR(11))
 BEGIN
     DECLARE name VARCHAR(32);
     DECLARE surname VARCHAR(32);
@@ -107,14 +107,14 @@ BEGIN
     COMMIT;
 END $$
 
-CREATE OR REPLACE PROCEDURE addCitizenPassword (pesel VARCHAR(11), password VARCHAR(32))
+CREATE OR REPLACE PROCEDURE addCitizenPassword(pesel VARCHAR(11), password VARCHAR(32))
 BEGIN
     PREPARE stmt FROM 'INSERT INTO passwords VALUES (?, ?)';
     EXECUTE stmt USING pesel, password;
     DEALLOCATE PREPARE stmt;
 END $$
 
-CREATE OR REPLACE PROCEDURE addCitizenStatus (pesel VARCHAR(11), status VARCHAR(32))
+CREATE OR REPLACE PROCEDURE addCitizenStatus(pesel VARCHAR(11), status VARCHAR(32))
 BEGIN
     PREPARE stmt FROM 'INSERT INTO statuses VALUES (?, ?)';
     EXECUTE stmt USING pesel, status;
@@ -129,27 +129,18 @@ BEGIN
     DEALLOCATE PREPARE stmt;
 END $$
 
-CREATE OR REPLACE PROCEDURE addCitizenGender (pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE addCitizenGender(pesel VARCHAR(11))
 BEGIN
-    DECLARE gender VARCHAR(32) DEFAULT extractGender(pesel);
-
-    PREPARE stmt FROM 'INSERT INTO genders VALUES (?, ?)';
-    EXECUTE stmt USING pesel, gender;
-    DEALLOCATE PREPARE stmt;
+    INSERT INTO genders VALUES (pesel, extractGender(pesel));
 END $$
 
-CREATE OR REPLACE PROCEDURE addCitizenBirthday (pesel VARCHAR(11))
+CREATE OR REPLACE PROCEDURE addCitizenBirthday(pesel VARCHAR(11))
 BEGIN
-    DECLARE dayOfBirth INT DEFAULT extractDayOfBirth(pesel);
-    DECLARE monthOfBirth INT DEFAULT extractMonthOfBirth(pesel);
-    DECLARE yearOfBirth INT DEFAULT extractYearOfBirth(pesel);
-
-    PREPARE stmt FROM 'INSERT INTO birthdays VALUES (?, ?, ?, ?)';
-    EXECUTE stmt USING pesel, dayOfBirth, monthOfBirth, yearOfBirth;
-    DEALLOCATE PREPARE stmt;
+    INSERT INTO birthdays VALUES (pesel, extractDayOfBirth(pesel),
+                                  extractMonthOfBirth(pesel), extractYearOfBirth(pesel));
 END $$
 
-CREATE OR REPLACE PROCEDURE deleteCitizen (pesel VARCHAR(11), status VARCHAR(32))
+CREATE OR REPLACE PROCEDURE deleteCitizen(pesel VARCHAR(11), status VARCHAR(32))
 BEGIN
     IF (citizenExists(pesel) AND getStatus(pesel) = status) THEN
         PREPARE stmt FROM 'DELETE FROM citizens WHERE pesel = ?';
@@ -159,75 +150,6 @@ BEGIN
     ELSE
         SELECT 'This citizen does not exist or cant be deleted.';
     END IF;
-END $$
-
-CREATE OR REPLACE PROCEDURE updateCityStats (oldCity VARCHAR(32), newCity VARCHAR(32))
-BEGIN
-    SET AUTOCOMMIT = 0;
-    START TRANSACTION;
-
-    UPDATE citystats
-        SET population = population - 1
-        WHERE name = oldCity;
-    UPDATE citystats
-        SET population = population + 1
-        WHERE name = newCity;
-
-    COMMIT;
-END $$
-
-CREATE OR REPLACE PROCEDURE deleteCitizenData (delPesel VARCHAR(11))
-BEGIN
-    SET AUTOCOMMIT = 0;
-    START TRANSACTION;
-
-    DELETE FROM passwords WHERE pesel = delPesel;
-    DELETE FROM statuses WHERE pesel = delPesel;
-
-    UPDATE yearstats YS
-        JOIN birthdays B ON YS.year = B.yearOfBirth
-        SET YS.quantity = YS.quantity - 1
-        WHERE B.pesel = delPesel;
-    DELETE FROM birthdays WHERE pesel = delPesel;
-
-    UPDATE genderstats GS
-        JOIN genders G ON GS.gender = G.gender
-        SET GS.quantity = GS.quantity - 1
-        WHERE pesel = delPesel;
-    DELETE FROM genders WHERE pesel = delPesel;
-
-    UPDATE citystats CS
-        JOIN addresses A ON CS.name = A.city
-        SET CS.population = CS.population - 1
-        WHERE A.pesel = delPesel;
-    DELETE FROM addresses WHERE pesel = delPesel;
-
-    COMMIT;
-END $$
-
-CREATE OR REPLACE PROCEDURE extractCitizenData (newPesel VARCHAR(11))
-BEGIN
-    SET AUTOCOMMIT = 0;
-    START TRANSACTION;
-
-    UPDATE citystats CS
-        JOIN addresses A ON CS.name = A.city
-        SET CS.population = CS.population + 1
-        WHERE A.pesel = newPesel;
-
-    CALL addCitizenGender(newPesel);
-    UPDATE genderstats GS
-        JOIN genders G ON GS.gender = G.gender
-        SET GS.quantity = GS.quantity + 1
-        WHERE G.pesel = newPesel;
-
-    CALL addCitizenBirthday(newPesel);
-    UPDATE yearstats YS
-        JOIN birthdays B ON YS.year = B.yearOfBirth
-        SET YS.quantity = YS.quantity + 1
-        WHERE B.pesel = newPesel;
-
-    COMMIT;
 END $$
 
 DELIMITER ;
--- Triggers.sql
+++ Triggers.sql
@@ -3,19 +3,82 @@ DELIMITER $$
 CREATE OR REPLACE TRIGGER tUpdateCityStats AFTER UPDATE ON addresses
 FOR EACH ROW
 BEGIN
-    CALL updateCityStats(OLD.city, NEW.city);
+    UPDATE citystats
+        SET population = population - 1
+        WHERE name = OLD.city;
+
+    IF (cityExists(NEW.city) = 0) THEN
+        CALL addCity(NEW.city);
+    END IF;
+
+    UPDATE citystats
+        SET population = population + 1
+        WHERE name = NEW.city;
+END $$
+
+CREATE OR REPLACE TRIGGER tAddCity BEFORE INSERT ON addresses
+FOR EACH ROW
+BEGIN
+    IF (cityExists(NEW.city) = 0) THEN
+        CALL addCity(NEW.city);
+    END IF;
+
+    UPDATE citystats CS
+        SET CS.population = CS.population + 1
+        WHERE name = NEW.city;
 END $$
 
 CREATE OR REPLACE TRIGGER tDeleteCitizenData BEFORE DELETE ON citizens
 FOR EACH ROW
 BEGIN
-    CALL deleteCitizenData(OLD.pesel);
+    DECLARE delPesel VARCHAR(11) DEFAULT OLD.pesel;
+
+    DELETE FROM passwords WHERE pesel = delPesel;
+    DELETE FROM statuses WHERE pesel = delPesel;
+
+    UPDATE yearstats YS
+        JOIN birthdays B ON YS.year = B.yearOfBirth
+        SET YS.quantity = YS.quantity - 1
+        WHERE B.pesel = delPesel;
+    DELETE FROM birthdays WHERE pesel = delPesel;
+
+    UPDATE genderstats GS
+        JOIN genders G ON GS.gender = G.gender
+        SET GS.quantity = GS.quantity - 1
+        WHERE pesel = delPesel;
+    DELETE FROM genders WHERE pesel = delPesel;
+
+    UPDATE citystats CS
+        JOIN addresses A ON CS.name = A.city
+        SET CS.population = CS.population - 1
+        WHERE A.pesel = delPesel;
+    DELETE FROM addresses WHERE pesel = delPesel;
 END $$
+
+DELIMITER $$
 
 CREATE OR REPLACE TRIGGER tExtractCitizenData AFTER INSERT ON citizens
 FOR EACH ROW
 BEGIN
-    CALL extractCitizenData(NEW.pesel);
+    DECLARE newPesel VARCHAR(11) DEFAULT NEW.pesel;
+
+    CALL addCitizenGender(newPesel);
+
+    UPDATE genderstats GS
+        JOIN genders G ON GS.gender = G.gender
+        SET GS.quantity = GS.quantity + 1
+        WHERE G.pesel = newPesel;
+
+    IF (yearExists(extractYearOfBirth(newPesel)) = 0) THEN
+        CALL addYear(extractYearOfBirth(newPesel));
+    END IF;
+
+    CALL addCitizenBirthday(newPesel);
+
+    UPDATE yearstats YS
+        JOIN birthdays B ON YS.year = B.yearOfBirth
+        SET YS.quantity = YS.quantity + 1
+        WHERE B.pesel = newPesel;
 END $$
 
 DELIMITER ;
